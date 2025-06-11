6a6e009820730d9b0241ed72043fc0dca20f4335
alphaleadership <thomas.iniguez@free.fr>
2025-06-11 16:36:50 +0200

    Mise à jour automatique des fichiers

--- www/bundle.js
+++ www/bundle.js
@@ -2080,12 +2080,12 @@ class TournamentMatch {
 
   render() {
     this.node.innerHTML = this.template();
-
+    this.
     this.updateView();
   }
 
   updateView() {
-    
+
     if (!this.data) return;
 
     const form = this.node.querySelector('form');
