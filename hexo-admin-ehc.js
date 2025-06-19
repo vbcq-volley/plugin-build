3ed375018744966be04e96ab23c1797d1540df7d
alphaleadership <thomas.iniguez@free.fr>
2025-06-19 09:59:30 +0200

    Mise à jour automatique des fichiers

--- hexo-admin-ehc.js
+++ hexo-admin-ehc.js
@@ -56410,9 +56410,9 @@ ${err.stack}`);
                 team1Ref = teams[i]._id;
                 team2Ref = teams[i + 1]._id;
               } else {
-                console.log(matches.find((m) => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team1 === m._id || match.team2 === m._id)));
-                team1Ref = matches.find((m) => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team1 === m._id || match.team2 === m._id)).winner;
-                team2Ref = matches.find((m) => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team2 === m._id || match.team1 === m._id)).winner;
+                const previousRMatch = matches.filter((m) => m.round === roundTab[roundTab.indexOf(currentRound) - 1]);
+                team1Ref = previousRMatch.find((m) => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team1 === m._id || match.team2 === m._id)).winner;
+                team2Ref = previousRMatch.find((m) => m.round === roundTab[roundTab.indexOf(currentRound) - 1] && !roundMatches.some((match) => match.team2 === m._id || match.team1 === m.id)).winner;
               }
               roundMatches.push({
                 team1: team1Ref,
